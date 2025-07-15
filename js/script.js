5948cf37212d942c8f35b9c7b924d6c162fabe13
raneesh2737 <raneeshmohammed2419@gmail.com>
2025-07-15 17:46:22 +0530

    responsive-start-991px

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $('scroll-bt').click(function(){
     $('html').scrolltop(0);
 })
+$(document).ready(function(){
+    $('.menu-bt').click(function(){
+        $('header').toggleClass('clickhead');
+        $('.menu-bt').toggleClass('toggled');
+        $('header nav').toggleClass('toggled');
+    })
+})
